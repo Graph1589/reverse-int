039267675cfd619283e95053bd54aecaa8772000
graph1589 <graph1589@gmail.com>
2021-03-09 22:45:02 +0300

    feat: solv reversing int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  const result = String(Math.abs(n)).split('').filter((n) => n >= 0 && n <= 9).reduce((acc, number) => [number, ...acc], []).join('').toString();
+  return Number(`${result}`);
 }
